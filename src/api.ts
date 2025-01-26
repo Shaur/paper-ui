ce729ed6c7c1c02b2e4b49d0be9e7d592ae2ba69
glamazdo <glamazdo.work@gmail.com>
2025-01-26 08:52:33 +0300

    updated: use nodeport and env variables

--- src/api.ts
+++ src/api.ts
@@ -1,29 +1,32 @@
 import axios from "axios";
 import {ApproveRequest} from "./comics/model";
 
+let serverUrl = process.env.SERVER_URL
+
 export function rejectPurgatoryItem(id: Number, callback:(id: Number) => void) {
     let token = localStorage.getItem("token")
-    axios.delete(`http://localhost:8080/private/comics/purgatory/${id}`, {headers: {"Authorization": "Bearer " + token}})
+
+    axios.delete(`${serverUrl}/private/comics/purgatory/${id}`, {headers: {"Authorization": "Bearer " + token}})
         .then(_ => callback(id))
 }
 
 export function approvePurgatoryItem(request: ApproveRequest, callback: (id: Number) => void) {
-    axios.put(`http://localhost:8080/private/comics/purgatory`, request, _headers())
+    axios.put(`${serverUrl}/private/comics/purgatory`, request, _headers())
         .then(_ => callback(request.id))
 }
 
 export function findBySeriesTitle(title: String, callback: (data: any) => void) {
-    axios.get(`http://localhost:8080/series/autocomplete?title=${title}`, _headers())
+    axios.get(`${serverUrl}/series/autocomplete?title=${title}`, _headers())
         .then(response => callback(response.data))
 }
 
 export function findSeries(callback: (data: any) => void) {
-    axios.get(`http://localhost:8080/series`, _headers())
+    axios.get(`${serverUrl}/series`, _headers())
         .then(response => callback(response.data))
 }
 
 export function getIssues(seriesId: Number, callback: (data: any) => void) {
-    axios.get(`http://localhost:8080/issue?seriesId=${seriesId}`, _headers())
+    axios.get(`${serverUrl}/issue?seriesId=${seriesId}`, _headers())
         .then(response => callback(response.data))
 }
 
