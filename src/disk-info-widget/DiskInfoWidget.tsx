3b52ee1edf581f8f8795e36956165f8f91831c64
glamazdo <glamazdo.work@gmail.com>
2025-05-02 08:46:54 +0300

    fixed: disk space numbers view

--- src/disk-info-widget/DiskInfoWidget.tsx
+++ src/disk-info-widget/DiskInfoWidget.tsx
@@ -2,9 +2,11 @@ import {LinearProgress, Stack} from "@mui/material";
 import {DiskInfoWidgetProperties} from "./DiskInfoWidgetProperties";
 
 function DiskInfoWidget(data: DiskInfoWidgetProperties) {
+    const usable = (data.diskInfo?.usable ?? 0.0).toFixed(2)
+    const total = (data.diskInfo?.total ?? 0).toFixed(2)
     return (
         <Stack direction='column' textAlign='center' width={150}>
-            <div>{data.diskInfo?.usable ?? 0.0}GB of {data.diskInfo?.total ?? 0}GB</div>
+            <div>{usable}GB of {total}GB</div>
             <LinearProgress
                 variant='determinate'
                 value={(data.diskInfo !== undefined) ? data.diskInfo?.usable / data.diskInfo?.total * 100 : 0}
